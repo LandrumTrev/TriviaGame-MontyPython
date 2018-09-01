1d2ca289da224f0ceae1da822e982da787316899
Rich Trevillian <landrum.trevillian@gmail.com>
2018-08-31 21:50:07 -0400

    logo placement

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,6 @@
-// Monty Python's Flying Trivia JS
+// Monty Python's Flying Trivia
+// basic trivia game assignment
+// © Richard Trevillian, 2018-09-01
+// University of Richmond, Full Stack Web Development Bootcamp
+// JavaScript
+
