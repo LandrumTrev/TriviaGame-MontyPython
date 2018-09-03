75e56a43faf0a400986b3f54c2b40985c3b9726d
Rich Trevillian <landrum.trevillian@gmail.com>
2018-09-03 13:30:30 -0400

    basic buttons functionality

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,6 +4,29 @@
 // University of Richmond, Full Stack Web Development Bootcamp
 // JavaScript
 
+// ACTIONS PERFORMED WHEN CLICKING START BUTTON ON HOME PAGE
 $('#btn_start').click(function () {
-    location.reload();
+
+    $('#start_box').attr('style', 'display:none;');
+    $('#quiz_box').attr('style', 'display:block;');
+    // console.log('hi!');
+
+});
+
+// ACTIONS PERFORMED WHEN CLICKING DONE BUTTON ON QUIZ PAGE
+$('#btn_done').click(function () {
+
+    $('#quiz_box').attr('style', 'display:none;');
+    $('#results_box').attr('style', 'display:block;');
+    // console.log('hi!');
+
+});
+
+// ACTIONS PERFORMED WHEN CLICKING PLAY AGAIN BUTTON ON RESULTS PAGE
+$('#btn_playAgain').click(function () {
+
+    $('#results_box').attr('style', 'display:none;');
+    $('#start_box').attr('style', 'display:block;');
+    // console.log('hi!');
+
 });
